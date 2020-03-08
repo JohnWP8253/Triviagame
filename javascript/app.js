ad0a8b846840fcc3c43e3c8c9dc3a67a861b2c63
John Pendergrass <john.pendergrass@me.com>
2020-03-08 14:43:01 -0500

    finished quotes variable with properties

--- javascript/app.js
+++ javascript/app.js
@@ -4,27 +4,54 @@ $(document).ready(function () {
     var quotes = [{
         quote:"You're gonna need a bigger boat.",
         answers: ["Overboard", "Jaws", "Titanic", "Master and Commander"],
-        correctAnswer:"Jaws"
+        correctAnswer:"Jaws",
+        gif:"https://media.giphy.com/media/aq6wETurhpbc4/giphy.gif"
     }, {
         quote:"You're killing me, Smalls",
         answers: ["The Sandlot", "The Bad News Bears", "Rookie of the Year", "The Perfect Game"],
-        correctAnswer:"The Sandlot"
+        correctAnswer:"The Sandlot",
+        gif:"https://media.giphy.com/media/3oeHLdMOyYLnjZ2CfC/giphy.gif"
     }, {
         quote:"Hasta la vista, baby!",
         answers: ["The Three Amigos", "The Punisher", "Spanglish", "The Terminator"],
-        correctAnswer:"The Terminator"
+        correctAnswer:"The Terminator",
+        gif:"https://media.giphy.com/media/26BoCW1FA2980EaR2/giphy.gif"
     }, {
         quote:"I'll be right here.",
         answers: ["Sixteen Candles", "Flubber", "E.T. The Extraterrestrial", "Star Wars"],
-        correctAnswer:"E.T. The Extraterrestrial"
+        correctAnswer:"E.T. The Extraterrestrial",
+        gif:"https://media.giphy.com/media/nL4HKlfoUBW7kO0vqu/giphy.gif"
     }, {
         quote:"I'm your number one fan.",
         answers: ["Crossroads", "Misery", "A Star is Born", "Dreamgirls"],
-        correctAnswer:"Misery"
+        correctAnswer:"Misery",
+        gif:"https://media.giphy.com/media/rpRmBGR3xaAZa/giphy.gif"
+    
     }, {
         quote:"I'm gonna make him an offer he can't refuse.",
         answers: ["The Godfather", "Goodfellas", "The Departed", "Casino"],
-        correctAnswer:"The Godfather"
+        correctAnswer:"The Godfather",
+        gif:"https://media.giphy.com/media/RlH7GlyiGKJ1ciAbO0/giphy.gif"
+    }, {
+        quote:"Did you have a brain tumor for breakfast?",
+        answers: ["Halloween", "Judy", "Heathers", "Austin Powers"],
+        correctAnswer:"Heathers",
+        gif:"https://media.giphy.com/media/3owyuaFZR2muu2udIQ/giphy.gif"
+    }, {
+        quote:"HEY YOU GUYS!!!!",
+        answers: ["Bratz", "The Goonies", "Ted", "Her"],
+        correctAnswer:"The Goonies",
+        gif:"https://media.giphy.com/media/5YhFFUFq6ZTry/giphy.gif"
+    }, {
+        quote:"You talking to me?",
+        answers: ["Taxi Driver", "Look Who's Talking", "Soapdish", "Quiz Show"],
+        correctAnswer:"Taxi Driver",
+        gif:"https://media.giphy.com/media/3oEjHO6kED0gWOSpCU/giphy.gif"
+    }, {
+        quote:"You can't handle the truth!",
+        answers: ["Risky Business", "Top Gun", "A Few Good Men", "The Firm"],
+        correctAnswer:"A Few Good Men",
+        gif:"https://media.giphy.com/media/IgsXOXGPxfT3O/giphy.gif"
     }, 
 ]
 }
