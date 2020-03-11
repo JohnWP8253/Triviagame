340cc37001471e10dc67210206eb8e17da6e5dad
John Pendergrass <john.pendergrass@me.com>
2020-03-11 16:55:00 -0500

    added css elements

--- javascript/app.js
+++ javascript/app.js
@@ -16,53 +16,53 @@ $(document).ready(function (){
 
     var movieTrivia = [{
         
-        quote:"You're gonna need a bigger boat.",
+        quote:"'You're gonna need a bigger boat.'",
         answers: ["Overboard", "Jaws", "Titanic", "Master and Commander"],
         correctAnswer:"1",
         gif:"https://media.giphy.com/media/aq6wETurhpbc4/giphy.gif"
     }, {
-        quote:"You're killing me, Smalls",
+        quote:"'You're killing me, Smalls!'",
         answers: ["The Sandlot", "The Bad News Bears", "Rookie of the Year", "The Perfect Game"],
         correctAnswer:"0",
         gif:"https://media.giphy.com/media/3oeHLdMOyYLnjZ2CfC/giphy.gif"
     }, {
-        quote:"Hasta la vista, baby!",
+        quote:"'Hasta la vista, baby!'",
         answers: ["The Three Amigos", "The Punisher", "Spanglish", "The Terminator"],
         correctAnswer:"3",
         gif:"https://media.giphy.com/media/26BoCW1FA2980EaR2/giphy.gif"
     }, {
-        quote:"I'll be right here.",
+        quote:"'I'll be right here.'",
         answers: ["Sixteen Candles", "Flubber", "E.T. The Extraterrestrial", "Star Wars"],
         correctAnswer:"2",
         gif:"https://media.giphy.com/media/nL4HKlfoUBW7kO0vqu/giphy.gif"
     }, {
-        quote:"I'm your number one fan.",
+        quote:"'I'm your number one fan.'",
         answers: ["Crossroads", "Misery", "A Star is Born", "Dreamgirls"],
         correctAnswer:"1",
         gif:"https://media.giphy.com/media/rpRmBGR3xaAZa/giphy.gif"
     
     }, {
-        quote:"I'm gonna make him an offer he can't refuse.",
+        quote:"'I'm gonna make him an offer he can't refuse.'",
         answers: ["The Godfather", "Goodfellas", "The Departed", "Casino"],
         correctAnswer:"0",
         gif:"https://media.giphy.com/media/RlH7GlyiGKJ1ciAbO0/giphy.gif"
     }, {
-        quote:"Did you have a brain tumor for breakfast?",
+        quote:"'Did you have a brain tumor for breakfast?'",
         answers: ["Halloween", "Judy", "Heathers", "Austin Powers"],
         correctAnswer:"2",
         gif:"https://media.giphy.com/media/3owyuaFZR2muu2udIQ/giphy.gif"
     }, {
-        quote:"HEY YOU GUYS!!!!",
+        quote:"'HEY YOU GUYS!!!!'",
         answers: ["Bratz", "The Goonies", "Ted", "Her"],
         correctAnswer:"1",
         gif:"https://media.giphy.com/media/5YhFFUFq6ZTry/giphy.gif"
     }, {
-        quote:"You talking to me?",
+        quote:"'You talking to me?'",
         answers: ["Taxi Driver", "Look Who's Talking", "Soapdish", "Quiz Show"],
         correctAnswer:"0",
         gif:"https://media.giphy.com/media/3oEjHO6kED0gWOSpCU/giphy.gif"
     }, {
-        quote:"You can't handle the truth!",
+        quote:"'You can't handle the truth!'",
         answers: ["Risky Business", "Top Gun", "A Few Good Men", "The Firm"],
         correctAnswer:"2",
         gif:"https://media.giphy.com/media/IgsXOXGPxfT3O/giphy.gif"
@@ -141,7 +141,7 @@ $(document).ready(function (){
 
     function resetQuote() {
         $(".answersAll").remove();
-        $(".answers").append('<div class=answerGif src"' + movieTrivia[indexQandA].gif + '">');
+        $(".answers").append('<img class=answerGif  width="auto" height="auto" src"' + movieTrivia[indexQandA].gif + '">');
         indexQandA++;
         if (indexQandA < movieTrivia.length) {
             setTimeout(function (){
